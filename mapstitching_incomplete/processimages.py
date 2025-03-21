a89cd2b1e8b593cf59d33f2a044ce12b09afc444
Remote VSCoder <remotevscoder@mothernewbee.com>
2025-03-21 18:17:09 +0800

    adding map stuff

---
+++ mapstitching_incomplete/processimages.py
@@ -0,0 +1,11 @@
+# load image from path
+
+# crop image to game area
+
+# check image for dialogue (if dialogue, ignore)
+
+# check image for teleport patches (if teleport, add new level)
+
+# check pairwise images for overlap
+
+# stitch images together based on overlap
